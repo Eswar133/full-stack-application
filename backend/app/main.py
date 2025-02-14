61d57218a76ec9c830a7a8a82e691e015548b461
Eswar133 <manikantapadala358@gmail.com>
2025-02-14 20:40:10 +0530

    reslove conflict between jwt and pyjwt[crypto]

--- backend/app/main.py
+++ backend/app/main.py
@@ -75,4 +75,5 @@ async def generate_numbers():
 # ✅ Run FastAPI Server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
